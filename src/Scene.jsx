9640052bf1936d147f207357e9b2b261789b2152
Akash Mulye <akashmulye97@gmail.com>
2023-06-08 14:38:43 -0400

    added new model

--- src/Scene.jsx
+++ src/Scene.jsx
@@ -1,22 +1,52 @@
-import { Suspense, useRef, useEffect } from "react";
-import { Canvas, useFrame } from "@react-three/fiber";
-import { Environment, OrbitControls, PerspectiveCamera } from "@react-three/drei";
-import { useGLTF, OrthographicCamera } from "@react-three/drei";
-import { PointerLockControls } from "@react-three/drei";
-import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader";
+import { Suspense, useState, useEffect } from "react";
+import { Canvas } from "@react-three/fiber";
+import { Environment, OrbitControls, useGLTF } from "@react-three/drei";
 
-import MODEL from "./assets/models/TamilArchive02.gltf";
+import MODEL from "./assets/models/TamilArchive04.gltf";
 import "./assets/scene.css";
 
+/*
+    Morning: time >= 5am and time <= 12pm
+    afternoon: time >= 12pm and time <= 5pm
+    Evening: time >= 5pm and time <= 9pm
+    Night: time >= 9pm and time <= 4am
+*/
+
 const Scene = () => {
-    
+    const [time, setTime] = useState(0);
+    const [dayState, setDayState] = useState("");
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            let hr = new Date().getHours();
+            setTime(hr);
+
+            let hours = parseInt(document.getElementById("time").innerHTML);
+            
+            if (hours >= 5 && hours < 12) {
+                setDayState("MORNING");
+            } else if (hours >= 12 &&  hours < 17) {
+                setDayState("AFTERNOON");
+            } else if (hours >= 17 && hours < 20) {
+                setDayState("EVENING");
+            } else {
+                setDayState("NIGHT");
+            }
+
+            let partOfDay = document.getElementById("day-state").innerHTML;
+        }, 1000);
+
+        return () => {
+            clearInterval(interval);
+        }
+    }, []);
+
     const Model = () => {
         const obj = useGLTF(MODEL);
-        console.log(obj);
 
         return (
             <>
-                <mesh scale={1} position={[-80, -50, -50]}>
+                <mesh scale={1} position={[-80, -30, 250]}>
                     <primitive object={obj.scene} />
                 </mesh>
             </>
@@ -27,14 +57,15 @@ const Scene = () => {
         <>
             <div>
                 <Suspense>
-                    <Canvas frameloop="always" camera={{fov: 75, near: 0.1, far: 10000, position: [0, 0, 100]}}>
-                        <ambientLight intensity={1.25} />
+                    <Canvas frameloop="always" camera={{fov: 75, near: 0.1, far: 100000, position: [0, 0, 100]}}>
+                        <ambientLight intensity={0} />
                         <Model />
                         <Environment preset="sunset" />
                         <OrbitControls />
-                        {/* <PointerLockControls selector="canvas" moveSpeed={100} delta={true} /> */}
                     </Canvas>
                 </Suspense>
+                <p id="time">{time}</p>
+                <p id="day-state">{dayState}</p>
             </div>
         </>
     )
