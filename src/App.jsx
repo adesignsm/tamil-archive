b0653f48bdbec522fe74751f0a9bf25ec47b5546
Akash Mulye <akashmulye97@gmail.com>
2023-07-06 16:05:56 -0400

    loading fix

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ const App = () => {
 
     return (
         <>
-            <LoadingScreen />
+            {isLoading === true ? <LoadingScreen /> : null}
             <Scene />
         </>
     )
